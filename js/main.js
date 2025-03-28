ebbec7dbd93a1c2876ffa77f85ee3d6d4bd39e0a
NicolasMuskusTarazona <nicolasmuskus1@gmail.com>
2025-03-28 09:13:34 -0500

    feat: :sparkles: `Edit a product.`
    main.js

--- js/main.js
+++ js/main.js
@@ -24,3 +24,30 @@ while(confirm("¿ Deasea insertar un dato ?")) {
     .then(result => alert(JSON.stringify(result)))
     .catch(error => alert(error))
 }
+
+const editProduct = async(data) =>{
+    const {id, ...dataUpdate} = data
+    const url = new URL("https://67e686856530dbd311105617.mockapi.io")
+    url.pathname = `/products/${id}`
+    const header = new Headers()
+    header.append("Content-Type", "application/json")
+    const config = {
+        method: "PUT", //Actualizar
+        headers: header,
+        body: JSON.stringify(dataUpdate)
+    }
+    const reponse = await fetch(url.toString(),config)
+    const result = await reponse.json()
+    return result
+}
+
+while (confirm("¿ Deseas actualizar un producto?")){
+    const data = {}
+    data.id = prompt("Ingrese el id del producto a actualizar", "Mic330")
+    data.name = (confirm("¿Desea actualizar el nonmbre del producto ?")) ? prompt("Ingrese el nonbre del producto") : undefined
+    if(confirm("¿Desea actulizar el precio del producto?")) data.price = Number(prompt("Ingrese el nuevo precio del producto"))
+    if(confirm("¿Desea actualizar la categoria del producto?")) data.category = prompt("Ingrese la nueva categoria del producto")
+    editProduct(data)
+    .then(result => alert(JSON.stringify(result)))
+    .catch(error => alert(error))
+}
