8202178ffc4c2ff0488b6914672657cc23aa47f3
NicolasMuskusTarazona <nicolasmuskus1@gmail.com>
2025-03-30 18:46:05 -0500

    feat: :bug: fix address and html wrapping
    `main.js and HTML`

--- js/main.js
+++ js/main.js
@@ -1,17 +1,95 @@
+const saveProduct = async(data) => {
+    const url = "https://67e686856530dbd311105617.mockapi.io/products"
+    const header = new Headers()
+    header.append("Content-Type", "application/json")
+    const config = {
+        method: "POST",
+        headers: header,
+        body: JSON.stringify(data)
+    }
+    const response = await fetch(url, config)
+    return response.json()
+}
+
+const editProduct = async(data) => {
+    const { id, ...dataUpdate } = data
+    const url = `https://67e686856530dbd311105617.mockapi.io/products/${id}`
+    const header = new Headers()
+    header.append("Content-Type", "application/json")
+    const config = {
+        method: "PUT",
+        headers: header,
+        body: JSON.stringify(dataUpdate)
+    }
+    const response = await fetch(url, config)
+    return response.json()
+}
+
+const deleteProduct = async(id) => {
+    const url = `https://67e686856530dbd311105617.mockapi.io/products/${id}`
+    const header = new Headers()
+    header.append("Content-Type", "application/json")
+    const config = {
+        method: "DELETE",
+        headers: header
+    }
+    const response = await fetch(url, config)
+    return response.json()
+}
+
+const saveUser = async(data) => {
+    const url = "https://67e686856530dbd311105617.mockapi.io/users"
+    const header = new Headers()
+    header.append("Content-Type", "application/json")
+    const config = {
+        method: "POST",
+        headers: header,
+        body: JSON.stringify(data)
+    }
+    const response = await fetch(url, config)
+    return response.json()
+}
+
+const editUser = async(data) => {
+    const { id, ...dataUpdate } = data
+    const url = `https://67e686856530dbd311105617.mockapi.io/users/${id}`
+    const header = new Headers()
+    header.append("Content-Type", "application/json")
+    const config = {
+        method: "PUT",
+        headers: header,
+        body: JSON.stringify(dataUpdate)
+    }
+    const response = await fetch(url, config)
+    return response.json()
+}
+
+const deleteUser = async(id) => {
+    const url = `https://67e686856530dbd311105617.mockapi.io/users/${id}`
+    const header = new Headers()
+    header.append("Content-Type", "application/json")
+    const config = {
+        method: "DELETE",
+        headers: header
+    }
+    const response = await fetch(url, config)
+    return response.json()
+}
+
 let opcion
 do {
     opcion =Number(prompt(
-    "-------------------------------------------------------------------------------\n" +
-    "Opciones de Producto: \n" + 
+        "------------------------------------------------------\n" +
+        "Opciones de Producto: \n" + 
     "   1. Añadir producto.\n" +
     "   2. Editar Producto.\n" +
     "   3. Eliminar Producto.\n" +
-    "-------------------------------------------------------------------------------\n" +
+    "------------------------------------------------------\n" +
     "Opciones de Usuario: \n" +
     "   4. Añadir Usuario.\n" +
     "   5. Editar Usuario.\n" +
     "   6. Eliminar Usuario.\n" +
-    "-------------------------------------------------------------------------------\n" +
+    "------------------------------------------------------\n" +
     "Seleccione una opcion:"
 ));
 } while (isNaN(opcion) || opcion < 1 || opcion > 6);
@@ -21,7 +99,7 @@ if (opcion === 1) {
         id: Number(prompt("Ingresa el id", "6")),
         name: prompt("Ingrese el nombre", "Mac mini"),
         price: Number(prompt("Ingrese el precio", 600)),
-        category: prompt("Ingrese la categoría", "Technology"),
+        category: prompt("Ingrese la categoria", "Technology"),
     };
 
     saveProduct(data)
@@ -33,9 +111,13 @@ else if (opcion === 2) {
     data.id = Number(prompt("Ingrese el ID del producto a actualizar", 1));
 
     if (!data.id) {
-        alert("ID inválido");
-        return;
+        alert("ID invalido");
+    } else {
+        if (confirm("¿Desea actualizar el nombre del producto?")) {
+            data.name = prompt("Ingrese el nuevo nombre del producto");
+        }
     }
+    
 
     if (confirm("¿Desea actualizar el nombre del producto?")) {
         data.name = prompt("Ingrese el nuevo nombre del producto");
@@ -64,9 +146,9 @@ else if (opcion === 2) {
 else if (opcion === 3) {
     const id = Number(prompt("Ingrese el ID del producto a eliminar", 1));
 
-    if (!id) {
-        alert("ID inválido");
-        return;
+    if (!data.id) {
+        alert("ID invalido");
+    } else {
     }
 
     if (confirm(`¿Está seguro de eliminar el producto con ID ${id}?`)) {
@@ -99,8 +181,8 @@ else if (opcion === 5) {
     user.id = Number(prompt("Ingrese el ID del usuario a actualizar", 1));
 
     if (!user.id) {
-        alert("ID inválido");
-        return;
+        alert("ID invalido");
+    } else {
     }
 
     if (confirm("¿Desea actualizar el nombre del usuario?")) {
@@ -128,11 +210,11 @@ else if (opcion === 6) {
     const id = Number(prompt("Ingrese el ID del usuario a eliminar", 1));
 
     if (!id) {
-        alert("ID inválido");
-        return;
+        alert("ID invalido");
+    } else {
     }
 
-    if (confirm(`¿Está seguro de eliminar el usuario con ID ${id}?`)) {
+    if (confirm(`¿Esta seguro de eliminar el usuario con ID ${id}?`)) {
         deleteUser(id)
             .then(result => {
                 console.log("Usuario eliminado:", result);
@@ -144,99 +226,6 @@ else if (opcion === 6) {
             });
     }
 } 
-
 else {
     alert("Opción no válida. Inténtalo de nuevo.");
-}
-const saveProduct = async(data) => {
-    const url = new URL("https://67e686856530dbd311105617.mockapi.io")
-    url.pathname = "/products"
-    const header = new Headers()
-    header.append("Content-Type", "application/json")
-    const config = {
-        method: "POST", //Guardar
-        headers: header,
-        body: JSON.stringify(data)
-    }
-    const reponse = await fetch(url.toString(),config)
-    const result = await reponse.json()
-    return result
-}
-const editProduct = async(data) =>{
-    const {id, ...dataUpdate} = data
-    const url = new URL("https://67e686856530dbd311105617.mockapi.io")    
-    url.pathname = `/products/${id}`
-    const header = new Headers()
-    header.append("Content-Type", "application/json")
-    const config = {
-        method: "PUT", //Actualizar
-        headers: header,
-        body: JSON.stringify(dataUpdate)
-    }
-    const reponse = await fetch(url.toString(),config)
-    const result = await reponse.json()
-    return result
-}
-
-const deleteProduct = async(data) =>{
-    const {id, ...dataUpdate} = data
-    const url = new URL("https://67e686856530dbd311105617.mockapi.io")
-    url.pathname = `/products/${id}`
-    const header = new Headers()
-    header.append("Content-Type", "application/json")
-    const config ={
-        method: "DELETE",
-        headers: header,
-        body: JSON.stringify(dataUpdate)
-}
-    const reponse = await fetch(url.toString(),config)
-    const result = await reponse.json()
-    return result
-}
-
-const saveUser = async(data) => {
-    const url = new URL("https://67e686856530dbd311105617.mockapi.io")
-    url.pathname = "/users"
-    const header = new Headers()
-    header.append("Content-Type", "application/json")
-    const config = {
-        method: "POST", //Guardar
-        headers: header,
-        body: JSON.stringify(data)
-}
-    const reponse = await fetch(url.toString(),config)
-    const result = await reponse.json()
-    return result
-}
-
-const editUser = async(data) =>{
-    const {id, ...dataUpdate} = data
-    const url = new URL("https://67e686856530dbd311105617.mockapi.io")
-    url.pathname = `/users/${id}`
-    const header = new Headers()
-    header.append("Content-Type", "application/json")
-    const config = {
-        method: "PUT", //Actualizar
-        headers: header,
-        body: JSON.stringify(dataUpdate)
-}
-    const reponse = await fetch(url.toString(),config)
-    const result = await reponse.json()
-    return result
-}
-
-const deleteUser = async(data) =>{
-    const {id, ...dataUpdate} = data
-    const url = new URL("https://67e686856530dbd311105617.mockapi.io")
-    url.pathname = `/users/${id}`
-    const header = new Headers()
-    header.append("Content-Type", "application/json")
-    const config ={
-        method: "DELETE",
-        headers: header,
-        body: JSON.stringify(dataUpdate)
-}
-    const reponse = await fetch(url.toString(),config)
-    const result = await reponse.json()
-    return result
 }
